d7fcad40ca2d0700854685b37388dcfd52a858f3
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2021-11-03 15:54:22 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav'
+import About from './views/About'
+import Exercices from './views/Exercises'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Nav/>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h3>Logo</h3>
+      <ul className='nav-links'>
+          <li>About</li>
+          <li>Shop</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
