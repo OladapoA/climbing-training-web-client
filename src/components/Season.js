7a275380d707d6e9d2c64bb98d571978013636ab
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2021-11-18 17:59:20 +0000

    updates

--- src/components/Season.js
+++ src/components/Season.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Week from './Week';
+import DaySessionModal from './DaySessionModal';
+import Button from '@mui/material/Button';
 
 function Season(props) {
-
+    const [open, setOpen] = useState(false);
+    const [daySessionId, setdaySessionId] = useState();
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleDayIdChange = (id) => setdaySessionId(id);
     const daySessions = props.season.daySessions
     var weeks = [];
+
+    console.log(daySessionId)
+
     if (daySessions !== undefined) {
         for (let i = 0; i < daySessions.length; i+=7) {
             var weekDaySessions = daySessions.slice(i, i+7);
@@ -14,6 +23,8 @@ function Season(props) {
                     key={weekNumber} 
                     weekNumber={weekNumber} 
                     weekDaySessions={weekDaySessions}
+                    handleOpen={handleOpen}
+                    handleDayIdChange={handleDayIdChange}
                 />
             );
         }
@@ -22,6 +33,12 @@ function Season(props) {
     return (
         <div>
             {/* <Week weekNumber={1}/> */}
+            {/* <Button onClick={handleOpen}>Open modal</Button> */}
+            <DaySessionModal 
+                handleOpen={handleOpen}
+                handleClose={handleClose}
+                open={open}
+            />
             {weeks}
         </div>
     );
--- src/components/Week.js
+++ src/components/Week.js
@@ -2,21 +2,22 @@ import React from 'react';
 import './Week.css';
 
 function Week(props) {
-  const { weekNumber, weekDaySessions } = props;
+  const { weekNumber, weekDaySessions, handleOpen, handleDayIdChange } = props;
   const today = new Date();
   today.setHours(0)
   today.setMinutes(0)
   today.setSeconds(0)
 
   var dayColours = []
+  // console.log(weekDaySessions)
 
   for (let daySession of weekDaySessions) {
     const sessionDate = Date.parse(daySession[1])
     // console.log(typeof Date.parse(daySession[1]))
     if (sessionDate < today.getTime()) {
-      console.log(daySession)
+      // console.log(daySession)
       const status = daySession[2]
-      console.log(status)
+      // console.log(status)
       if (status === null) {
         dayColours.push("pastIncomplete")
       } else {
@@ -37,15 +38,14 @@ function Week(props) {
         <div className="day todayFinished">F</div>
         <div className="day today">S</div>
         <div className="day future">S</div> */}
-
         <div className="title"><h2>Week {weekNumber}</h2></div>
-        <div className={`day ${dayColours[0]}`}>M</div>
-        <div className={`day ${dayColours[1]}`}>T</div>
-        <div className={`day ${dayColours[2]}`}>W</div>
-        <div className={`day ${dayColours[3]}`}>T</div>
-        <div className={`day ${dayColours[4]}`}>F</div>
-        <div className={`day ${dayColours[5]}`}>S</div>
-        <div className={`day ${dayColours[6]}`}>S</div>
+        <div onClick={() => handleDayIdChange(weekDaySessions[0][0])} className={`day ${dayColours[0]}`}>M</div>
+        <div onClick={handleOpen} className={`day ${dayColours[1]}`}>T</div>
+        <div onClick={handleOpen} className={`day ${dayColours[2]}`}>W</div>
+        <div onClick={handleOpen} className={`day ${dayColours[3]}`}>T</div>
+        <div onClick={handleOpen} className={`day ${dayColours[4]}`}>F</div>
+        <div onClick={handleOpen} className={`day ${dayColours[5]}`}>S</div>
+        <div onClick={handleOpen} className={`day ${dayColours[6]}`}>S</div>
     </div>
   );
 }
