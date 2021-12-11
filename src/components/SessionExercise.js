e589578e9ae01466df5340d4e1181bebc3762940
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2021-12-11 12:49:46 +0000

    updates

--- src/components/SessionExercise.js
+++ src/components/SessionExercise.js
@@ -8,7 +8,31 @@ function SessionExercise(props) {
 
     const [visible, setVisible] = useState(false);
 
-    const handleClick = () => setVisible(!visible)
+    const handleClick = () => setVisible(!visible);
+
+    const handleAction = (action) => {
+        
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+        };
+        fetch(`/api/v1/session-exercises/${sessionExercise.id}/status/?status=${action}`, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+        // window.location.reload(false);
+    };
+
+    const ActionButton = () => {
+        const status = sessionExercise.status;
+        var buttonText = "";
+        if (status === null) {
+            buttonText = "Start";
+        } else if (status === false) {
+            buttonText = "Finish";
+        }
+        return <Button>{buttonText}</Button>
+    }
 
     return (
         <div className="App">
@@ -25,6 +49,10 @@ function SessionExercise(props) {
             }
             <h4>Duration: {sessionExercise.time}</h4>
             <h4>Status: <Status status={sessionExercise.status} /></h4>
+            { sessionExercise.exercise !== null &&
+                // <Button>{sessionExercise.status === null ? "Start" : "Finish" }</Button>
+                <ActionButton />
+            }
             <hr />
         </div>
     );
